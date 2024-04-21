a4c4dd0794596412246fb34cc49b4b93f9ade644
Kevin Pandu <162404229+Infoonlygit@users.noreply.github.com>
2024-04-21 14:09:48 +0700

    Create Looping.php

---
+++ Looping.php
@@ -0,0 +1,14 @@
+#!/usr/bin/php
+
+<?php
+// Tentukan jumlah iterasi
+$jumlah_iterasi = 1500;
+
+// Pesan yang ingin dicetak
+$pesan = "BAPAK KAU BELI TV SEKEN";
+
+// Looping menggunakan for
+for ($i = 0; $i < $jumlah_iterasi; $i++) {
+    echo $pesan . "<br>";
+}
+?>
